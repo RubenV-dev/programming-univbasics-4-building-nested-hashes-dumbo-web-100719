ec2760eb35ca30372103f5bd43956439f8592974
Ruben Vallejo <r.will16@hotmail.com>
2019-09-24 02:40:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,9 +27,7 @@ def monopoly_with_third_tier
       :two_piece_owned => 50,
       :three_piece_owned => 100,
       :four_piece_owned =>200
-    },{:names => {}
-    
-      }
+    }
  }}
 	hash
 end

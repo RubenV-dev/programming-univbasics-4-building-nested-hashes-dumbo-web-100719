9cb98ef02887815e08589acbeeb625698c3f48c9
Ruben Vallejo <r.will16@hotmail.com>
2019-09-24 02:58:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,9 +29,11 @@ def monopoly_with_third_tier
       :four_piece_owned =>200
     }
     ,{:names => {
-      
-    }
-      
+      :reading_railroad => {},
+      :pennsylvania_railroad => {},
+      :b_and_o_railroad => {},
+      :shortline_railroad => {}
+      }
     }
           }
   }
